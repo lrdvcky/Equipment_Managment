2dd138f4ee55ae54a7fb6e890e57f13f605251fa
lrdvcky <lordovcky2006@gmail.com>
2025-05-16 09:58:06 +0500

    Главная страница

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Информационная система учета оборудования</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="wrapper">
+
+    <header>
+    <div class="header-content">
+        <img src="img/logo.png" alt="Логотип" class="logo">
+        <h1>Система учёта оборудования</h1>
+    </div>
+</header>
+
+
+    <nav>
+        <a href="#">Оборудование</a>
+        <a href="#">Пользователи</a>
+        <a href="#">Программы</a>
+        <a href="#">Инвентаризация</a>
+        <a href="#">Расходники</a>
+    </nav>
+
+    <main>
+        <h2 class="highlight">Добро пожаловать!</h2>
+        <p>Это информационная система учёта оборудования и расходных материалов учебного заведения.</p>
+        <p>Выберите модуль из меню выше, чтобы начать работу.</p>
+        <button class="red-button">Перейти к оборудованию</button>
+    </main>
+
+    <footer>
+        &copy; 2025 Учебное заведение. Все права защищены.
+    </footer>
+
+</div>
+
+</body>
+</html>
